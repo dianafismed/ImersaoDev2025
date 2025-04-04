8e92f0db2c453a1007a5d76b02a8ee57add13ab3
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2025-04-03 21:39:41 -0300

    up

--- js.js
+++ js.js
@@ -1,3 +1,10 @@
+function converter() {
+  const wons = 1000;
+  let reais = parseFloat(prompt("Digite o valor em reais: "));
+  alert("O valor convertido é de: " + wons * reais + " wons");
+}
+
+
 function jogo() {
   jogador = prompt("Escolha: \n 1 - Pedra \n 2 - Papel \n 3 - Tesoura \n");
   computador = Math.floor(Math.random() * 3) + 1;
@@ -19,5 +26,24 @@ function jogo() {
     alert("Você perdeu!");
   } else {
     alert("Escolha inválida!");
+  }
+}
+
+function pegada() {
+  let rodada = 1;
+  while (rodada <= 3) {
+    //alert("Rodada " + rodada);
+    jogador = prompt("Nível " + rodada + "\nEscolha o vidro: 1, 2 ou 3");
+    vidro = Math.floor(Math.random() * 3) + 1;
+    if(jogador == vidro){
+      alert("O vidro quebrou ! Fim de jogo !");
+      rodada = 1000;
+    } else {
+      alert("O vidro não quebrou ! Você pode continuar jogando !");
+    }
+    rodada++;
+  }
+  if(rodada == 4){
+    alert("Você venceu ! Parabéns !");
   }
 }
