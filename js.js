81c788c826e18dd432a578671af0ea4a27385a8a
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2025-04-03 22:18:24 -0300

    up

--- js.js
+++ js.js
@@ -4,7 +4,6 @@ function converter() {
   alert("O valor convertido é de: " + wons * reais + " wons");
 }
 
-
 function jogo() {
   jogador = prompt("Escolha: \n 1 - Pedra \n 2 - Papel \n 3 - Tesoura \n");
   computador = Math.floor(Math.random() * 3) + 1;
@@ -32,10 +31,9 @@ function jogo() {
 function pegada() {
   let rodada = 1;
   while (rodada <= 5) {
-    //alert("Rodada " + rodada);
     jogador = prompt("Nível " + rodada + "\nEscolha o vidro: 1, 2, 3, 4 ou 5 ");
     vidro = Math.floor(Math.random() * 5) + 1;
-    if(jogador == vidro){
+    if (jogador == vidro) {
       alert("O vidro quebrou ! Fim de jogo !");
       rodada = 1000;
     } else {
@@ -43,7 +41,49 @@ function pegada() {
     }
     rodada++;
   }
-  if(rodada == 4){
+
+  if (rodada == 4) {
     alert("Você venceu ! Parabéns !");
+  }
+}
+
+function jogar() {
+  let herois = ["", "", ""];
+  let viloes = ["", "", ""];
+  let forcaHerois = 0;
+  let forcaViloes = 0;
+
+  for (let i = 0; i < 3; i++) {
+    heroi = prompt("Digite o nome do heroi: " + (i + 1));
+    herois[i] = heroi;
+    forcaHerois += Math.floor(Math.random() * 10) + 1;
+  }
+
+  for (let i = 0; i < 3; i++) {
+    aleatorio = Math.floor(Math.random() * 10);
+    possiveis = [
+      "Thanos",
+      "Loki",
+      "Ultron",
+      "Hela",
+      "Doutor Octopus",
+      "Venom",
+      "Carnificina",
+      "Duende Verde",
+      "Mysterio",
+      "Doutor Destino",
+    ];
+    viloes[i] = possiveis[aleatorio];
+    forcaViloes += Math.floor(Math.random() * 10) + 1;
+  }
+
+  alert("Herois: " + herois + "\nViloes: " + viloes);
+  alert("Força dos Herois: " + forcaHerois + "\nForça dos Viloes: " + forcaViloes);
+  if (forcaHerois == forcaViloes) {
+    alert("Deu empate !");
+  } else if (forcaHerois > forcaViloes) {
+    alert("Os Herois venceram !");
+  } else {
+    alert("Os Viloes venceram !");
   }
 }
